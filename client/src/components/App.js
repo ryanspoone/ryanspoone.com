e09aad221190ae4c0f8a4f78f12fc6dcbf023031
Ryan Spoone <ryanspoone@gmail.com>
2020-08-09 20:39:53 -0500

    Add this repo stats to footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import _ from 'lodash';
 
 import Header from './Header';
 import Home from './Home';
@@ -8,19 +9,77 @@ import SocialSidebar from './SocialSidebar';
 import EmailSidebar from './EmailSidebar';
 import NotFound from './NotFound';
 
-const App = () => (
-    <div className="layout">
-        <Header />
-        <SocialSidebar />
-        <EmailSidebar />
+export default class App extends Component {
+    constructor(props) {
+        super(props);
 
-        <Switch>
-            <Route exact path="/" component={Home} />
-            <Route component={NotFound} />
-        </Switch>
+        this.state = {
+            github: {
+                repo: {
+                    error: undefined,
+                    data: undefined,
+                    isLoading: true
+                }
+            }
+        };
+    }
 
-        <Footer />
-    </div>
-);
+    componentDidMount() {
+        try {
+            this.getRepoStats();
+        } catch (err) {
+            let error = _.get(err, 'message') || _.get(err, 'error', err);
+            if (_.isEmpty(error)) {
+                error = 'An error status was returned but no error message.';
+            }
+            if (!_.isString(error)) {
+                error = JSON.stringify(error);
+            }
+            console.error('Thrown error from server:', error);
+        }
+    }
 
-export default App;
+    async getRepoStats() {
+        const response = await fetch('/api/github');
+        const data = await response.json();
+        if (response.status !== 200) {
+            const error = _.get(data, 'error', data);
+            this.setState({
+                githubRepo: {
+                    error,
+                    data: undefined,
+                    isLoading: false
+                }
+            });
+        } else {
+            this.setState({
+                githubRepo: {
+                    data,
+                    error: undefined,
+                    isLoading: false
+                }
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="layout">
+                <Header />
+                <SocialSidebar />
+                <EmailSidebar />
+
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route component={NotFound} />
+                </Switch>
+
+                <Footer
+                    data={_.get(this.state, 'githubRepo.data')}
+                    error={_.get(this.state, 'githubRepo.error')}
+                    isLoading={_.get(this.state, 'githubRepo.isLoading')}
+                />
+            </div>
+        );
+    }
+}
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,8 +4,9 @@ import _ from 'lodash';
 
 export default class Footer extends Component {
     static propTypes = {
-        githubForks: PropTypes.number,
-        githubStars: PropTypes.number
+        data: PropTypes.object,
+        isLoading: PropTypes.bool,
+        error: PropTypes.any
     };
 
     render() {
@@ -28,7 +29,7 @@ export default class Footer extends Component {
                                     d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"
                                 ></path>
                             </svg>
-                            <span>{_.get(this, 'props.githubStars', 0)}</span>
+                            <span>{_.get(this, 'props.data.githubStars', 0)}</span>
                         </span>
                         <span>
                             <svg
@@ -44,7 +45,7 @@ export default class Footer extends Component {
                                     d="M8 1a1.993 1.993 0 0 0-1 3.72V6L5 8 3 6V4.72A1.993 1.993 0 0 0 2 1a1.993 1.993 0 0 0-1 3.72V6.5l3 3v1.78A1.993 1.993 0 0 0 5 15a1.993 1.993 0 0 0 1-3.72V9.5l3-3V4.72A1.993 1.993 0 0 0 8 1zM2 4.2C1.34 4.2.8 3.65.8 3c0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3 10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2zm3-10c-.66 0-1.2-.55-1.2-1.2 0-.65.55-1.2 1.2-1.2.65 0 1.2.55 1.2 1.2 0 .65-.55 1.2-1.2 1.2z"
                                 ></path>
                             </svg>
-                            <span>{_.get(this, 'props.githubForks', 0)}</span>
+                            <span>{_.get(this, 'props.data.githubForks', 0)}</span>
                         </span>
                     </div>
                 </a>

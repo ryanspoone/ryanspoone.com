4d33da50097d584d66ab367006d984bfd7a00255
Ryan Spoone <ryanspoone@gmail.com>
2020-08-10 01:18:06 -0500

    Update footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,9 +11,33 @@ export default class Footer extends Component {
 
     render() {
         return (
-            <footer className="mastfoot mt-auto text-center">
-                <a href="https://github.com/ryanspoone">
-                    <div>Designed &amp; Built by Ryan Spoone</div>
+            <footer className="foot">
+                <a href="https://github.com/ryanspoone/ryanspoone.com">
+                    <div>
+                        Built with{' '}
+                        <span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="14px"
+                                height="14px"
+                                viewBox="0 0 14 14"
+                                version="1.1"
+                            >
+                                <g id="surface1">
+                                    <path
+                                        style={{
+                                            stroke: 'none',
+                                            fillRule: 'evenodd',
+                                            fill: 'rgb(100, 255, 218)',
+                                            fillOpacity: 1
+                                        }}
+                                        d="M 7 12.597656 C 3.714844 9.363281 0.582031 6.589844 0.582031 4.195312 C 0.582031 1.984375 2.371094 1.167969 3.664062 1.167969 C 4.429688 1.167969 6.085938 1.457031 7 3.765625 C 7.925781 1.453125 9.605469 1.171875 10.339844 1.171875 C 11.820312 1.171875 13.417969 2.117188 13.417969 4.195312 C 13.417969 6.566406 10.421875 9.226562 7 12.597656 M 10.339844 0.589844 C 9.054688 0.589844 7.746094 1.195312 7 2.476562 C 6.25 1.191406 4.945312 0.582031 3.664062 0.582031 C 1.808594 0.582031 0 1.859375 0 4.195312 C 0 6.914062 3.25 9.695312 7 13.417969 C 10.75 9.695312 14 6.914062 14 4.195312 C 14 1.855469 12.195312 0.589844 10.339844 0.589844 "
+                                    />
+                                </g>
+                            </svg>
+                        </span>{' '}
+                        by Ryan Spoone
+                    </div>
                     <div className="github-stars-forks">
                         <span>
                             <svg

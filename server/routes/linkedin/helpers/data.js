c769678ef50e59f5e03731b53c0c56004ccde38f
Ryan Spoone <me@ryanspoone.com>
2021-10-21 20:03:55 -0500

    Update jobs

--- server/routes/linkedin/helpers/data.js
+++ server/routes/linkedin/helpers/data.js
@@ -1,10 +1,9 @@
 export default {
     'three-current-positions': [
         {
-            title: 'Senior Software Engineer',
-            summary:
-                'Scrum Technical Lead<br /><ul><li>Technical leader for various parts of our product such as integrations, actions, goals, internationalization, & data-pipeline.</li><li>Implemented new operations that allowed us to reduce the number of external API calls by half by using data that already was in the system.</li><li>Launched custom object types allowing customers to sync one-off tables.</li><li>Created a calendar service that allowed customers and our system to accurately bucket and display data relevant for their custom calendar schedule.</li><li>Developed an integration framework that allowed our system to navigate lossless data on deploys.</li></ul>',
-            'start-date': '2019',
+            title: 'Principal Software Engineer',
+            summary: '',
+            'start-date': '2021',
             'end-date': null,
             'is-current': true,
             company: 'InsightSquared',
@@ -12,6 +11,16 @@ export default {
         }
     ],
     'three-past-positions': [
+        {
+            title: 'Senior Software Engineer',
+            summary:
+                'Scrum Technical Lead<br /><ul><li>Technical leader for various parts of our product such as integrations, actions, goals, internationalization, & data-pipeline.</li><li>Implemented new operations that allowed us to reduce the number of external API calls by half by using data that already was in the system.</li><li>Launched custom object types allowing customers to sync one-off tables.</li><li>Created a calendar service that allowed customers and our system to accurately bucket and display data relevant for their custom calendar schedule.</li><li>Developed an integration framework that allowed our system to navigate lossless data on deploys.</li></ul>',
+            'start-date': '2019',
+            'end-date': '2021',
+            'is-current': false,
+            company: 'InsightSquared',
+            'company-url': 'https://www.insightsquared.com'
+        },
         {
             title: 'Senior Software Engineer',
             summary:
@@ -28,16 +37,6 @@ export default {
                 'Consultant<br /><ul><li>Developed a performance testing tool to be open sourced for x86_64 Linux-based systems.</li><li>The tool benchmarked the cache latency, memory latency and bandwidth, cryptography throughput, compression/decompression speed, floating-point performance, compilation speed, NoSQL and SQL database throughput, and concurrent container performance.</li></ul>',
             'start-date': '2016',
             'end-date': '2017',
-            'is-current': false,
-            company: 'Intel Corporation',
-            'company-url': 'https://www.intel.com'
-        },
-        {
-            title: 'Performance Engineer',
-            summary:
-                'Datacenter Performance and Competitive Marketing<br /><ul><li>Company-wide owner of multiple performance benchmarks that included general purpose compute workloads, memory bandwidth, high performance computing, and web workloads.</li><li>Increased the number of benchmarks completed for Intel and competitive processors by 1200% through automation.</li><li>Engineered an enterprise web application for processor performance comparison that achieved a user return rate of 90%.</li></ul>',
-            'start-date': '2014',
-            'end-date': '2016',
             'is-current': false,
             company: 'Intel Corporation',
             'company-url': 'https://www.intel.com'

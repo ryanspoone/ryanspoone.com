b8aa52505cee9a5e66e9b0dcea9e3d9452a7e74f
Ryan Spoone <me@ryanspoone.com>
2024-03-13 13:08:53 -0500

    Update Contact to focus more on EM

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -9,9 +9,11 @@ export default function Contact() {
             <h4 className="title">Get In Touch</h4>
             <div>
                 <p>
-                    Although I&apos;m not currently looking for any new opportunities, my inbox is always
-                    open. Whether you have a question or just want to say hi, I&apos;ll try my best to get
-                    back to you!
+                    While I am always on the lookout for challenges that stimulate growth and innovation, my
+                    primary mission is to build and lead teams that leave a lasting impact. Whether
+                    you&apos;re interested in discussing potential collaborations, leadership strategies, or
+                    just want to share insights, my inbox is always open. I&apos;m eager to connect and
+                    explore how we can drive technology forward together!
                 </p>
             </div>
             <a className="btn btn-primary" href="mailto:me@ryanspoone.com">

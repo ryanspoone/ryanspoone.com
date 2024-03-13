8e5e7ca48e4586579dec8c2c30270cba887a0c7f
Ryan Spoone <me@ryanspoone.com>
2024-03-13 13:10:19 -0500

    Remove GitHub stats

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -4,20 +4,12 @@ import PropTypes from 'prop-types';
 import get from 'lodash/get';
 
 import '../styles/Projects.css';
-import GitHubStats from './GitHubStats.js';
 import FeaturedProjects from './FeaturedProjects.js';
 
-export default function Projects({ githubFeatured, githubUser } = {}) {
+export default function Projects({ githubFeatured } = {}) {
     return (
         <section id="projects" className="projects">
             <h3 className="heading">What I&apos;ve Been Up To</h3>
-            <h4>Some Stats From GitHub</h4>
-            <GitHubStats
-                data={get(githubUser, 'data')}
-                error={get(githubUser, 'error')}
-                errorCode={get(githubUser, 'errorCode')}
-                isLoading={get(githubUser, 'isLoading')}
-            />
             <h4>Some Things I&apos;ve Built</h4>
             <FeaturedProjects
                 data={get(githubFeatured, 'data')}

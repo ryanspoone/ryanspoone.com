1e79ae6af1c43ecc4f0706213622e9dfdb9c618e
Ryan Spoone <me@ryanspoone.com>
2024-03-13 13:21:54 -0500

    Update job history

--- server/routes/linkedin/helpers/data.js
+++ server/routes/linkedin/helpers/data.js
@@ -1,45 +1,46 @@
 export default {
     'three-current-positions': [
         {
-            title: 'Principal Software Engineer',
-            summary: '',
-            'start-date': '2021',
-            'end-date': null,
-            'is-current': true,
-            company: 'InsightSquared',
-            'company-url': 'https://www.insightsquared.com'
+            title: 'Director of Engineering',
+            summary:
+                "<ul><li><b>Pioneered the Nearshore Engineering Team:</b> Launched Mediafly's first nearshore engineering team, doubling our development capacity. This strategic initiative played a critical role in enhancing team resilience and contributed to a sustained $4M ARR during significant organizational restructuring.</li><li><b>Drove Strategic Project Development:</b> Led the execution of key projects, achieving a 15% improvement in product delivery efficiency. This leadership directly contributed to enhancing Mediafly's market position and customer satisfaction.</li><li><b>Implemented Agile Frameworks:</b> Established robust agile practices, improving project predictability and delivery timelines, which led to a 20% increase in customer satisfaction scores.</li><li><b>Championed Diversity and Inclusion:</b> Spearheaded a diversity and inclusion initiative within the engineering department, achieving a 30% increase in diversity hires, enriching our team culture and innovation capacity.</li></ul>",
+            'start-date': '2023',
+            'end-date': '2024',
+            'is-current': false,
+            company: 'Mediafly',
+            'company-url': 'https://www.mediafly.com'
         }
     ],
     'three-past-positions': [
         {
+            title: 'Engineering Manager',
+            summary:
+                '<ul><li><b>Cultivated a Culture of Innovation:</b> Managed and mentored a team of 15 engineers, leading to the development of key products that contributed to a $3M ARR. Fostered an environment of continuous improvement and innovation.</li><li><b>Enhanced Cross-Departmental Collaboration:</b> Streamlined communication with product management and design teams, accelerating product development cycles by 25%. This effort significantly improved collaboration and efficiency across departments.</li><li><b>Developed Comprehensive Onboarding:</b> Introduced an onboarding program for new engineers that halved ramp-up time, significantly boosting early productivity and team integration.</li></ul>',
+            'start-date': '2022',
+            'end-date': '2023',
+            'is-current': false,
+            company: 'Mediafly',
+            'company-url': 'https://www.mediafly.com'
+        },
+        {
+            title: 'Principal Engineer',
+            summary:
+                '<ul><li><b>Optimized System Architecture:</b> Enhanced system architecture, achieving a 60% increase in data processing efficiency. This initiative enabled handling 40% more data volume, showcasing my ability to optimize resources for scalability.</li><li><b>Mentored Junior Engineers:</b> Guided junior engineers in improving their technical skills and project ownership, which led to a 25% increase in team output, emphasizing the value of mentorship in team development.</li><li><b>Led Scalable Data Pipeline Design:</b> Designed and implemented a scalable data pipeline, improving data integrity and supporting key customer analytics features, underlining my technical leadership in creating robust solutions.</li>',
+            'start-date': '2021',
+            'end-date': '2022',
+            'is-current': false,
+            company: 'InsightSquared',
+            'company-url': 'https://www.insightsquared.com'
+        },
+        {
             title: 'Senior Software Engineer',
             summary:
-                'Scrum Technical Lead<br /><ul><li>Technical leader for various parts of our product such as integrations, actions, goals, internationalization, & data-pipeline.</li><li>Implemented new operations that allowed us to reduce the number of external API calls by half by using data that already was in the system.</li><li>Launched custom object types allowing customers to sync one-off tables.</li><li>Created a calendar service that allowed customers and our system to accurately bucket and display data relevant for their custom calendar schedule.</li><li>Developed an integration framework that allowed our system to navigate lossless data on deploys.</li></ul>',
+                '<ul><li><b>Innovated Integration Frameworks:</b> Developed a new integration framework, reducing external API dependency by 50%, demonstrating my ability to innovate for more reliable and scalable solutions.</li><li><b>Scrum Master Leadership:</b> Served as a scrum master, enhancing team efficiency and morale through effective sprint planning and backlog management, highlighting my leadership in agile environments.</li></ul>',
             'start-date': '2019',
             'end-date': '2021',
             'is-current': false,
             company: 'InsightSquared',
             'company-url': 'https://www.insightsquared.com'
-        },
-        {
-            title: 'Senior Software Engineer',
-            summary:
-                'Integrations Component Owner<br /><ul> <li>Integrated 5 new connectors (Office365, Zendesk, Jira, GitHub, Oracle Sales Cloud) into our services which led to securing 3x the current customers.</li><li>Led initiative on standardizing engineering documentation, creating a CLI, and other developer tools to decrease on-boarding time by 50%.</li><li>Implemented asynchronous and delta data retrieval which reduced integration synchronization time by 83%.</li><li>Redesigned and created a new priority queue manager for integration syncs to survive deploys.</li><li>Lead, abstracted, and refactored the main Salesforce integration into a generic CRM integration, decreasing development time of future CRM-based integrations by 75%.</li></ul>',
-            'start-date': '2018',
-            'end-date': '2019',
-            'is-current': false,
-            company: 'Olono (acquired by InsightSquared)',
-            'company-url': 'https://www.olono.ai'
-        },
-        {
-            title: 'Software Engineer',
-            summary:
-                'Consultant<br /><ul><li>Developed a performance testing tool to be open sourced for x86_64 Linux-based systems.</li><li>The tool benchmarked the cache latency, memory latency and bandwidth, cryptography throughput, compression/decompression speed, floating-point performance, compilation speed, NoSQL and SQL database throughput, and concurrent container performance.</li></ul>',
-            'start-date': '2016',
-            'end-date': '2017',
-            'is-current': false,
-            company: 'Intel Corporation',
-            'company-url': 'https://www.intel.com'
         }
     ]
 };

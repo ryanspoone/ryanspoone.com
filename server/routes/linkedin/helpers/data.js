921b8a10e25e6f651b8a9155ef05b17595116412
Ryan Spoone <me@ryanspoone.com>
2024-06-07 07:14:25 -0500

    Update data.js

--- server/routes/linkedin/helpers/data.js
+++ server/routes/linkedin/helpers/data.js
@@ -2,8 +2,14 @@ export default {
     'three-current-positions': [
         {
             title: 'Director of Engineering',
-            summary:
-                "<ul><li><b>Pioneered the Nearshore Engineering Team:</b> Launched Mediafly's first nearshore engineering team, doubling our development capacity. This strategic initiative played a critical role in enhancing team resilience and contributed to a sustained $4M ARR during significant organizational restructuring.</li><li><b>Drove Strategic Project Development:</b> Led the execution of key projects, achieving a 15% improvement in product delivery efficiency. This leadership directly contributed to enhancing Mediafly's market position and customer satisfaction.</li><li><b>Implemented Agile Frameworks:</b> Established robust agile practices, improving project predictability and delivery timelines, which led to a 20% increase in customer satisfaction scores.</li><li><b>Championed Diversity and Inclusion:</b> Spearheaded a diversity and inclusion initiative within the engineering department, achieving a 30% increase in diversity hires, enriching our team culture and innovation capacity.</li></ul>",
+            summary: `
+                <ul>
+                    <li><b>Pioneered the Nearshore Engineering Team:</b> Launched Mediafly's first nearshore engineering team, doubling our development capacity. This strategic initiative played a critical role in enhancing team resilience and contributed to a sustained $4M ARR during significant organizational restructuring.</li>
+                    <li><b>Drove Strategic Project Development:</b> Led the execution of key projects, achieving a 15% improvement in product delivery efficiency. This leadership directly contributed to enhancing Mediafly's market position and customer satisfaction.</li>
+                    <li><b>Implemented Agile Frameworks:</b> Established robust agile practices, improving project predictability and delivery timelines, which led to a 20% increase in customer satisfaction scores.</li>
+                    <li><b>Championed Diversity and Inclusion:</b> Spearheaded a diversity and inclusion initiative within the engineering department, achieving a 30% increase in diversity hires, enriching our team culture and innovation capacity.</li>
+                </ul>
+            `,
             'start-date': '2023',
             'end-date': '2024',
             'is-current': false,
@@ -14,8 +20,13 @@ export default {
     'three-past-positions': [
         {
             title: 'Engineering Manager',
-            summary:
-                '<ul><li><b>Cultivated a Culture of Innovation:</b> Managed and mentored a team of 15 engineers, leading to the development of key products that contributed to a $3M ARR. Fostered an environment of continuous improvement and innovation.</li><li><b>Enhanced Cross-Departmental Collaboration:</b> Streamlined communication with product management and design teams, accelerating product development cycles by 25%. This effort significantly improved collaboration and efficiency across departments.</li><li><b>Developed Comprehensive Onboarding:</b> Introduced an onboarding program for new engineers that halved ramp-up time, significantly boosting early productivity and team integration.</li></ul>',
+            summary: `
+                <ul>
+                    <li><b>Cultivated a Culture of Innovation:</b> Managed and mentored a team of 15 engineers, leading to the development of key products that contributed to a $3M ARR. Fostered an environment of continuous improvement and innovation.</li>
+                    <li><b>Enhanced Cross-Departmental Collaboration:</b> Streamlined communication with product management and design teams, accelerating product development cycles by 25%. This effort significantly improved collaboration and efficiency across departments.</li>
+                    <li><b>Developed Comprehensive Onboarding:</b> Introduced an onboarding program for new engineers that halved ramp-up time, significantly boosting early productivity and team integration.</li>
+                </ul>
+            `,
             'start-date': '2022',
             'end-date': '2023',
             'is-current': false,
@@ -24,8 +35,13 @@ export default {
         },
         {
             title: 'Principal Engineer',
-            summary:
-                '<ul><li><b>Optimized System Architecture:</b> Enhanced system architecture, achieving a 60% increase in data processing efficiency. This initiative enabled handling 40% more data volume, showcasing my ability to optimize resources for scalability.</li><li><b>Mentored Junior Engineers:</b> Guided junior engineers in improving their technical skills and project ownership, which led to a 25% increase in team output, emphasizing the value of mentorship in team development.</li><li><b>Led Scalable Data Pipeline Design:</b> Designed and implemented a scalable data pipeline, improving data integrity and supporting key customer analytics features, underlining my technical leadership in creating robust solutions.</li>',
+            summary: `
+                <ul>
+                    <li><b>Optimized System Architecture:</b> Enhanced system architecture, achieving a 60% increase in data processing efficiency. This initiative enabled handling 40% more data volume, showcasing my ability to optimize resources for scalability.</li>
+                    <li><b>Mentored Junior Engineers:</b> Guided junior engineers in improving their technical skills and project ownership, which led to a 25% increase in team output, emphasizing the value of mentorship in team development.</li>
+                    <li><b>Led Scalable Data Pipeline Design:</b> Designed and implemented a scalable data pipeline, improving data integrity and supporting key customer analytics features, underlining my technical leadership in creating robust solutions.</li>
+                </ul>
+            `,
             'start-date': '2021',
             'end-date': '2022',
             'is-current': false,
@@ -34,8 +50,12 @@ export default {
         },
         {
             title: 'Senior Software Engineer',
-            summary:
-                '<ul><li><b>Innovated Integration Frameworks:</b> Developed a new integration framework, reducing external API dependency by 50%, demonstrating my ability to innovate for more reliable and scalable solutions.</li><li><b>Scrum Master Leadership:</b> Served as a scrum master, enhancing team efficiency and morale through effective sprint planning and backlog management, highlighting my leadership in agile environments.</li></ul>',
+            summary: `
+                <ul>
+                    <li><b>Innovated Integration Frameworks:</b> Developed a new integration framework, reducing external API dependency by 50%, demonstrating my ability to innovate for more reliable and scalable solutions.</li>
+                    <li><b>Scrum Master Leadership:</b> Served as a scrum master, enhancing team efficiency and morale through effective sprint planning and backlog management, highlighting my leadership in agile environments.</li>
+                </ul>
+            `,
             'start-date': '2019',
             'end-date': '2021',
             'is-current': false,
